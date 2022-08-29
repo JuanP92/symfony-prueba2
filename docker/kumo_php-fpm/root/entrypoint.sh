f555c6174e93254733b609193862896864755b6f
Juan Pablo <jpabloariza93@gmail.com>
2022-08-29 16:49:26 -0500

    instalación de dependencias

---
+++ docker/kumo_php-fpm/root/entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -x
+mkdir -p /root/.ssh/
+
+if [[ ! -z $ON_ENTRY_SCRIPT ]] && [[ -f $ON_ENTRY_SCRIPT ]]
+then
+    $ON_ENTRY_SCRIPT
+fi
+
+exec /usr/bin/supervisord --nodaemon -c /etc/supervisor/supervisord.conf
